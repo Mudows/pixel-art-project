bb4273bc9903c7cba83a94f2533e80968c05943f
Diego C. Cezar <dmcezar@gmail.com>
2022-03-21 17:15:22 -0300

    Requisito 7 - Finalizado.

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
+const colorPicker = document.getElementsByClassName('color');
+
+for (let i = 0; i < colorPicker.length; i += 1) {
+  colorPicker[i].addEventListener('click', selectColor);
+}
+
+function selectColor(event) {
+  for (let i = 0; i < colorPicker.length; i += 1) {
+    colorPicker[i].classList.remove('selected');
+  }
+  event.target.classList.add('selected');
+}
+
 const pixelBoard = document.getElementById('pixel-board');
 let boardSize = 25;
 
-function createBoard () {
-    for (let i = 0; i < boardSize; i += 1){
-        const pixel = document.createElement('div');
-        pixel.setAttribute('data-id',i);
-        pixel.className += 'pixel';
-        pixelBoard.appendChild(pixel);
-    }
+function createBoard() {
+  for (let i = 0; i < boardSize; i += 1) {
+    const pixel = document.createElement('div');
+    pixel.setAttribute('data-id', i);
+    pixel.className += 'pixel';
+    pixelBoard.appendChild(pixel);
+  }
 }
 
-createBoard()
+createBoard();
